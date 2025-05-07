4704da5d585e7c3358ef54f014911064b11868d3
Victoran0 <victoranosunkvic@gmail.com>
2025-05-07 19:07:37 +0100

    initial commit

---
+++ KokoroApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using KokoroSharp;
+
+
+// string longText = @"This is a very long text that goes on and on... You can even span multiple lines using the @ symbol like this.";
+
+class Program
+{
+    static void Main()
+    {
+        var tts = KokoroTTS.LoadModel(); // Automatically downloads the model (~320MB)
+        var voice = KokoroVoiceManager.GetVoice("af_heart");
+        string tenMinuteText = File.ReadAllText("input.txt");
+        tts.SpeakFast(tenMinuteText, voice);
+
+        // Optional: keep the app running
+        Console.WriteLine("speaking the ten Minute Text. Press Ctrl + C to exit.");
+        Console.ReadKey();
+    }
+}
+
+
+
